8419783f293632258b5ce15c0737e620f9271bd3
Piper Merriam <pipermerriam@gmail.com>
2015-08-10 10:18:00 -0600

    improve coverage for parameter default

--- tests/loading/definition/parameters/single/test_default_validation.py
+++ tests/loading/definition/parameters/single/test_default_validation.py
@@ -1,5 +1,8 @@
 import pytest
 
+from flex.constants import (
+    ARRAY, STRING, MULTI,
+)
 from flex.error_messages import MESSAGES
 from flex.exceptions import ValidationError
 from flex.loading.definitions.parameters import (
@@ -44,4 +47,51 @@ def test_parameter_validation_with_default_present():
     assert_path_not_in_errors(
         'default',
         errors
+    )
+
+
+def test_default_as_array_is_valid():
+    """
+    https://github.com/swagger-api/swagger-spec/issues/343#issuecomment-128792251
+    """
+    context = {'deferred_references': set()}
+    parameter = ParameterFactory(
+        name="status",
+        type=ARRAY,
+        items={"type": STRING},
+        collectionFormat=MULTI,
+        default=["active"],
+    )
+    try:
+        single_parameter_validator(parameter, context=context)
+    except ValidationError as err:
+        errors = err.detail
+    else:
+        errors = {}
+
+    assert_path_not_in_errors(
+        'default',
+        errors
+    )
+
+
+def test_default_as_non_array_is_invalid():
+    """
+    https://github.com/swagger-api/swagger-spec/issues/343#issuecomment-128792251
+    """
+    context = {'deferred_references': set()}
+    parameter = ParameterFactory(
+        name="status",
+        type=ARRAY,
+        items={"type": STRING},
+        collectionFormat=MULTI,
+        default="active",
+    )
+    with pytest.raises(ValidationError) as err:
+        single_parameter_validator(parameter, context=context)
+
+    assert_message_in_errors(
+        MESSAGES['default']['invalid_type'],
+        err.value.detail,
+        'default',
     )
