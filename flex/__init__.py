2d8b48fdcf5940d6b8bf52d2b6560f3bbd167e92
Piper Merriam <pipermerriam@gmail.com>
2016-11-17 15:46:52 -0700

    6.0.1 Release

--- flex/__init__.py
+++ flex/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '6.0.0'
+VERSION = '6.0.1'
 
 from flex.core import load  # NOQA
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-version = '6.0.0'
+version = '6.0.1'
 
 readme = open(os.path.join(DIR, 'README.md')).read()
 
